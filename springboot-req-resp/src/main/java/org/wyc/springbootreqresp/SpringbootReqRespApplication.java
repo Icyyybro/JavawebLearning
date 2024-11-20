95f725190791cea43cc861e68656724fa8c9ce48
YiChuanWang <1425837933@qq.com>
2024-11-20 11:37:08 +0800

    springboot-req-resp

---
+++ springboot-req-resp/src/main/java/org/wyc/springbootreqresp/SpringbootReqRespApplication.java
@@ -0,0 +1,13 @@
+package org.wyc.springbootreqresp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootReqRespApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootReqRespApplication.class, args);
+    }
+
+}
---
+++ springboot-req-resp/src/test/java/org/wyc/springbootreqresp/SpringbootReqRespApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wyc.springbootreqresp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootReqRespApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
