9a5c5008d8291635fe4485aded415d8e2bd2a020
YiChuanWang <1425837933@qq.com>
2024-11-20 11:36:42 +0800

    springboot-req-resp

--- springboot-quickstart/src/main/java/org/wyc/springbootquickstart/controller/HelloController.java
+++ springboot-quickstart/src/main/java/org/wyc/springbootquickstart/controller/HelloController.java
@@ -1,7 +1,7 @@
 package org.wyc.springbootquickstart.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.  RestController;
 
 @RestController
 public class HelloController {
