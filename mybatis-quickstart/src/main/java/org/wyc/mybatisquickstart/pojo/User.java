d66891f76e18edfb169684b4960e68cf23b77489
YiChuanWang <1425837933@qq.com>
2024-11-23 22:26:05 +0800

    mysql

---
+++ mybatis-quickstart/src/main/java/org/wyc/mybatisquickstart/pojo/User.java
@@ -0,0 +1,61 @@
+package org.wyc.mybatisquickstart.pojo;
+
+public class User {
+    private Integer id;
+    private String name;
+    private Short age;
+    private short gender;
+    private String phone;
+
+    public User(){
+
+    }
+
+    public User(Integer id, String name, Short age, short gender, String phone) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.phone = phone;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Short getAge() {
+        return age;
+    }
+
+    public void setAge(Short age) {
+        this.age = age;
+    }
+
+    public short getGender() {
+        return gender;
+    }
+
+    public void setGender(short gender) {
+        this.gender = gender;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
